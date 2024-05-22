251c300e022af91a0b4436dc07e08dda18e3cfad
dragon3h <dragon3h@gmx.com>
2024-05-22 16:07:05 -0400

    build: added prettier and lint-staged

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,21 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2023: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:storybook/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended',
+    'prettier',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
-    semi: ["error", "always"],
+    semi: ['error', 'always'],
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Pokedex } from "./pages/Pokedex/Pokedex";
-import { Pokemon } from "./pages/Pokemon/Pokemon";
-import { fetchPokedexStart } from "./state/pokemon/pokemonSlice";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Pokedex } from './pages/Pokedex/Pokedex';
+import { Pokemon } from './pages/Pokemon/Pokemon';
+import { fetchPokedexStart } from './state/pokemon/pokemonSlice';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 const App = () => {
   const dispatch = useDispatch();
